bec658d472f1b73bac6ff38b837fba620df03b40
Andrew Maraev <the_vk@thevk.net>
2025-08-12 22:52:08 -0700

    add basic bench interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::time::{Instant};
+
+pub struct B {
+    current_iterations: u32,
+    target_iterations: u32,
+
+    suite_start_instant: Option<Instant>,
+    suite_end_instant: Option<Instant>,
+    iteration_instants: Vec<Instant>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl B {
+    fn new(target_iterations: u32) -> B {
+        B {
+            current_iterations: 0,
+            target_iterations,
+            suite_start_instant: None,
+            suite_end_instant: None,
+            iteration_instants: vec![]
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn n(&mut self) -> bool {
+        let instant = Instant::now();
+        self.iteration_instants.push(instant);
+        match self.current_iterations {
+            0 => self.suite_start_instant = Some(instant),
+            t if t == self.target_iterations => self.suite_end_instant = Some(instant),
+            _ => ()
+        }
+
+        self.current_iterations += 1;
+
+        println!("iterations: {} out of {}", self.current_iterations, self.target_iterations);
+        self.current_iterations <= self.target_iterations
+    }
+}
+
+pub fn suite<R, O>(name: &str, mut routine: R)
+    where
+        R : FnMut(&mut B) -> O,
+{
+    println!("bench {name}");
+
+    let mut b  = B::new(16);
+
+    routine(&mut b);
+
+    match (b.suite_start_instant, b.suite_end_instant) {
+        (Some(s), Some(e)) => {
+            println!("Total elapsed: {:?}", e.duration_since(s));
+        },
+        _ => ()
     }
 }
